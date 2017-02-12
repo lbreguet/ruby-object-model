416e696c843324b63274364b62a9ad6ce86d93ec
lbreguet <louis-w@live.com>
2017-02-11 19:35:18 -0500

    Ruby models intro
    -review modelling

--- lib/equine.rb
+++ lib/equine.rb
@@ -1,11 +1,30 @@
+# frozen_string_literal: true
+
 class Equine
+  def eat_grass
+    p 'munch munch munch '
+  end
 end
 
-class Horse
+module Ridable
+  def ride
+    p 'gallop gallop'
+  end
 end
 
-class Mule
+class Horse < Equine
+  include Ridable
 end
 
-class Zebra
+class Mule < Equine
+  include Ridable
+end
+
+class Zebra < Equine
+end
+
+module Ridable
+  def ride
+    p 'gallop gallop'
+  end
 end
